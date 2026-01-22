734cca9ce877002812517d9d67d74ec06eb43f35
Gabriel Maciel <gabriel54108916@edu.pa.senac.br>
2026-01-22 20:19:05 -0300

    AplicandoOpçõesPrincipais
    + image apenas para a categoria vídeo
    + image apenas para a categoria volume
    + aplicação de dotween

--- Assets/Grupos/DevGames/ShutGabriel/Script/MenuController.cs
+++ Assets/Grupos/DevGames/ShutGabriel/Script/MenuController.cs
@@ -15,9 +15,14 @@ public class MenuController : MonoBehaviour
     //DOTWEEN PARA BOTÕES
     void Start()
     {
+        CallMenu();
+    }
+
+    void CallMenu()
+    {
         for (int i = 0; i < menu1.Count; i++)
         {
-            menu1[i].transform.localScale = new Vector2 (0, 0);
+            menu1[i].transform.localScale = new Vector2(0, 0);
         }
         for (int i = 0; i < menu2.Count; i++)
         {
@@ -30,7 +35,7 @@ public class MenuController : MonoBehaviour
         for (int i = 0; i < menu1.Count; i++)
         {
             menu1[i].DOScale(2f, .5f);
-            yield return new WaitForSeconds(.25f);
+            yield return new WaitForSeconds(.50f);
         }
         
         for (int i = 0; i < menu2.Count; i++)
@@ -44,17 +49,6 @@ public class MenuController : MonoBehaviour
     {
         SceneManager.LoadScene(_nomeDoJogo);
     }
-    public void AbrirOpcoes()
-    {
-        _PainelMenuInicial.SetActive(false);
-        _PainelOpções.SetActive(true);
-
-    }
-    public void FecharOpcoes()
-    {
-        _PainelMenuInicial.SetActive(true);
-        _PainelOpções.SetActive(false);
-    }
     public void Sair()
     {
         Debug.Log("Saiu do Jogo");
@@ -62,12 +56,15 @@ public class MenuController : MonoBehaviour
     }
     // DG para Painel Opções
     public void Movepainel(string panel)
+
     {
+        CallMenu();
         GameObject.Find(panel).transform.DOLocalMove(new Vector3(0, 0, 0), .5f);
         _FocusPainel = panel;
     }
     public void BACKpainel(string panel)
     {
+        CallMenu();
         GameObject.Find(_FocusPainel).transform.DOLocalMove(new Vector3(1500, 0, 0), .5f);
     }
 }
