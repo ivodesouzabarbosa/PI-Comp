13589fd17a69363c60fdf689a265640c8050ae61
Leandro Pinheiro <Leandrob2612@Gmail.com>
2026-01-29 20:24:56 -0300

    DedoDuro

---
+++ Assets/Planta 3D Senac/Scripts/DedoDuro.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class DedoDuro : MonoBehaviour
+{
+    [MenuItem("Tools/Top 20 Objetos mais pesados do projeto")]
+    [System.Obsolete]
+    static void ListarPesados()
+    {
+        
+        MeshFilter[] todosOsMeshes = Object.FindObjectsOfType<MeshFilter>();
+
+        
+        var listaOrdenada = todosOsMeshes
+            .Where(mf => mf.sharedMesh != null)
+            .OrderByDescending(mf => mf.sharedMesh.triangles.Length)
+            .Take(20) 
+            .ToArray();
+
+        Debug.Log("TOP 20 OBJETOS VENCEDORES!");
+
+        foreach (var mf in listaOrdenada)
+        {
+            int tris = mf.sharedMesh.triangles.Length / 3;
+            Debug.Log($"🛑 <b>{mf.name}</b> | Tris: {tris:N0}", mf.gameObject);
+        }
+    }
+}
