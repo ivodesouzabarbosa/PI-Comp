bdafa923c2577037ef7a484bf837d69994d35c63
Gabriel Maciel <gabriel54108916@edu.pa.senac.br>
2026-02-12 18:49:23 -0300

    UpdateMoveUI

---
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Pinicial.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Pinicial : MonoBehaviour
+{
+    public RectTransform pontoInicial;
+    public float intervalo = 0.5f;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnWave), 0f, intervalo);
+    }
+
+    void SpawnWave()
+    {
+        GameObject wave = OndaPool.Instance.GetObject();
+        wave.SetActive(true);
+
+        RectTransform rect = wave.GetComponent<RectTransform>();
+        rect.position = pontoInicial.position;
+    }
+}
