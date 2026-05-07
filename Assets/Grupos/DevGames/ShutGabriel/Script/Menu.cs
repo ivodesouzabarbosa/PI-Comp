f907d39fec49760b9d0bb245427ee24c28161fbb
Gabriel Maciel <gabrielamaciel0307@gmail.com>
2026-05-07 20:37:06 -0300

    UpdateMenu
    +adição da função de troca de cena

---
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private string Cena;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(Cena);
+    }
+}
