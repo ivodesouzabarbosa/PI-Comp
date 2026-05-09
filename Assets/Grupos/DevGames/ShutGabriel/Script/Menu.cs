e5dcc2bab3125666f106ba46f75f3b4a564071a7
Gabriel Maciel <gabrielamaciel0307@gmail.com>
2026-05-09 11:55:54 -0300

    DebugLogADD

--- Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
@@ -5,9 +5,13 @@ using System.Collections.Generic;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private string Cena;
-
+    public void SelecionarCurso(string curso)
+    {
+        Debug.Log("Curso de " + curso + "Selecionado");
+    }
     public void Jogar()
     {
         SceneManager.LoadScene(Cena);
     }
+
 }
