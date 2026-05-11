8d3652910cdcd5ecc31d309a67f8df1a73479f77
IVO DE SOUZA BARBOSA <ivosouzabarbosa@gmail.com>
2026-05-10 23:11:52 -0300

    menu

--- Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
@@ -5,13 +5,35 @@ using System.Collections.Generic;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private string Cena;
-    public void SelecionarCurso(string curso)
+    public void SelecionarCurso(int value)
     {
-        Debug.Log("Curso de " + curso + "Selecionado");
+        switch (value)
+        {
+           case 0:
+            break;
+           case 1:
+            Debug.Log("Curso de " + "Fotografia" + " Selecionado");
+            break;
+           case 2:
+            Debug.Log("Curso de " + "Idomas" + " Selecionado");
+            break;
+           case 3:
+            Debug.Log("Curso de " + "Moda" + " Selecionado");
+            break;
+           case 4:
+            Debug.Log("Curso de " + "Analise de Dados" + " Selecionado");
+            break;
+           case 5:
+            Debug.Log("Curso de " + "Desenvolvimento de sistemas" + " Selecionado");
+            break;
+           case 6:
+            Debug.Log("Curso de " + "Manutenção de redes" + " Selecionado");
+            break;
+
+        }
+
+     
     }
-    public void Jogar()
-    {
-        SceneManager.LoadScene(Cena);
-    }
+  
 
 }
