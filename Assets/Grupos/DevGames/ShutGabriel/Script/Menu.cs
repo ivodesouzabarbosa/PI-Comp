bb7863556ca2925367b5d63877d291d2ec012f79
IVO DE SOUZA BARBOSA <ivosouzabarbosa@gmail.com>
2026-05-11 00:15:37 -0300

    Menu Inicial

--- Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
@@ -2,33 +2,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private string Cena;
+    [SerializeField] private GerenciadorObjetosMapa _gerenciadorObjetosMapa;
+    [SerializeField] Transform _menuIni;
     public void SelecionarCurso(int value)
     {
+        _menuIni.DOScale(0, .25f);
+        _gerenciadorObjetosMapa.EscolherObjeto(value);
         switch (value)
         {
+  
            case 0:
-            break;
+                Debug.Log("Curso de " + "Desenvolvimento de sistemas" + " Selecionado");
+                break;
            case 1:
-            Debug.Log("Curso de " + "Fotografia" + " Selecionado");
-            break;
+                Debug.Log("Curso de " + "Fotografia" + " Selecionado");
+                break;
            case 2:
-            Debug.Log("Curso de " + "Idomas" + " Selecionado");
-            break;
-           case 3:
-            Debug.Log("Curso de " + "Moda" + " Selecionado");
-            break;
-           case 4:
-            Debug.Log("Curso de " + "Analise de Dados" + " Selecionado");
-            break;
-           case 5:
-            Debug.Log("Curso de " + "Desenvolvimento de sistemas" + " Selecionado");
-            break;
-           case 6:
-            Debug.Log("Curso de " + "Manutenção de redes" + " Selecionado");
-            break;
+                Debug.Log("Curso de " + "Analise de Dados" + " Selecionado");
+                break;
+            case 3:
+                Debug.Log("Curso de " + "Idomas" + " Selecionado"); 
+                break;
+            case 4:
+                Debug.Log("Curso de " + "Moda" + " Selecionado");
+                break;
+            case 5:
+                Debug.Log("Curso de " + "Manutenção de redes" + " Selecionado");
+                break;
 
         }
 
