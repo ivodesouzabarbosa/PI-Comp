87b675eafe1a1bee437e1154d2b77aa6fa24ad6d
IVO DE SOUZA BARBOSA <ivosouzabarbosa@gmail.com>
2026-05-11 00:26:44 -0300

    log de ativar botão apos menu

--- Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
+++ Assets/Grupos/DevGames/ShutGabriel/Script/Menu.cs
@@ -12,31 +12,9 @@ public class Menu : MonoBehaviour
     {
         _menuIni.DOScale(0, .25f);
         _gerenciadorObjetosMapa.EscolherObjeto(value);
-        switch (value)
-        {
-  
-           case 0:
-                Debug.Log("Curso de " + "Desenvolvimento de sistemas" + " Selecionado");
-                break;
-           case 1:
-                Debug.Log("Curso de " + "Fotografia" + " Selecionado");
-                break;
-           case 2:
-                Debug.Log("Curso de " + "Analise de Dados" + " Selecionado");
-                break;
-            case 3:
-                Debug.Log("Curso de " + "Idomas" + " Selecionado"); 
-                break;
-            case 4:
-                Debug.Log("Curso de " + "Moda" + " Selecionado");
-                break;
-            case 5:
-                Debug.Log("Curso de " + "Manutenção de redes" + " Selecionado");
-                break;
+        Debug.Log("Ativar botoes do chão para navegar");
 
-        }
 
-     
     }
   
 
